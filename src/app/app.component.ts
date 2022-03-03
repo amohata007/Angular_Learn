bf82ab86d89ae6b240c62256413d3ce10ec037c8
amohata007 <amohata.007@gmail.com>
2022-03-03 16:12:00 +0530

    Components and Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstproject';
+  func(){
+    return this.title
+  }
+  fcheck(what){
+    alert(what)
+  }
+  obj = {
+    name : "Harry",
+    age : 20
+  }
+  arr = ['ironman','thor','blackpanther','hulk'];
+  a=10;
+  b=20;
+  variable = window.location.href
 }
